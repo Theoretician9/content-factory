1667c4d2ebea568d5adbdee7017cc551f7ab4984
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-29 00:41:19 +0500

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>APP STATIC TEST</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Landing route</div>} />
+        <Route path="/login" element={<div>Login route</div>} />
+        <Route path="/register" element={<div>Register route</div>} />
+        <Route path="/dashboard" element={<div>Dashboard route</div>} />
+      </Routes>
+    </Router>
   );
 };
 
