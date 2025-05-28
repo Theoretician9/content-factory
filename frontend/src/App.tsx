840505f94addbc0e8ddceb868ee978d0498fb2a6
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-29 00:52:17 +0500

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => (
-  <I18nextProvider i18n={i18n}>
-    <div>i18n test</div>
-  </I18nextProvider>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<div>Landing route</div>} />
+      <Route path="/login" element={<div>Login route</div>} />
+      <Route path="/register" element={<div>Register route</div>} />
+      <Route path="/dashboard" element={<div>Dashboard route</div>} />
+    </Routes>
+  </HashRouter>
 );
 
 export default App;
