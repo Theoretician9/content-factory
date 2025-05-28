59e285413bcde62933daaa0ed7b09ff80338f917
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-29 00:38:46 +0500

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import './App.css'
 
 const App = () => {
   return (
-    <I18nextProvider i18n={i18n}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  </I18nextProvider>
+    <div>APP STATIC TEST</div>
   );
 };
 
