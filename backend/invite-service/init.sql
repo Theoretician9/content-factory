ec9d95dd58602c555fcc19dbb275023558f7ce4a
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-06-28 21:52:01 +0500

    invite-service

--- backend/invite-service/init.sql
+++ backend/invite-service/init.sql
@@ -1,16 +1,8 @@
 -- Invite Service Database Initialization
--- Создание базы данных и пользователя для Invite Service
+-- База данных invite_db и пользователь invite_user уже созданы через переменные окружения
+-- Этот файл выполняется уже в контексте созданной базы данных
 
--- Создание пользователя
-CREATE USER invite_user WITH PASSWORD 'invite_password';
-
--- Создание базы данных
-CREATE DATABASE invite_db OWNER invite_user;
-
--- Подключение к базе данных invite_db
-\c invite_db;
-
--- Выдача всех прав пользователю invite_user на базу данных invite_db
+-- Устанавливаем права доступа для пользователя
 GRANT ALL PRIVILEGES ON DATABASE invite_db TO invite_user;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO invite_user;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO invite_user;
@@ -19,4 +11,7 @@ GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO invite_user;
 -- Установка прав по умолчанию для новых объектов
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO invite_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO invite_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO invite_user; 
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO invite_user;
+
+-- Создание расширений, если нужны
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; 
